95a795c0f95eba06e2e957951c822a1144dcecbf
Bernhard Wallisch <walliscb@technikum-wien.at>
2021-06-14 17:54:20 +0200

    initial commit

---
+++ ToiletSimulator/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using ToiletSimulator.Consumer;
+using ToiletSimulator.Producer;
+using ToiletSimulator.Queues;
+
+namespace ToiletSimulator {
+    class Program {
+        static void Main(string[] args) {
+
+            SimpleQueue queue = new SimpleQueue();
+
+            int randomSeed = new Random().Next();
+            Random random = new Random(randomSeed);
+
+            PeopleGenerator[] producers = new PeopleGenerator[Parameters.Producers];
+            for (int i = 0; i < producers.Length; i++) {
+                producers[i] = new PeopleGenerator("People Generator " + i, queue, random);
+            }
+
+            Toilet[] consumers = new Toilet[Parameters.Consumers];
+            for (int i = 0; i < consumers.Length; i++) {
+                consumers[i] = new Toilet("Toilet " + i, queue);
+            }
+
+            for (int i = 0; i < producers.Length; i++) {
+                producers[i].Produce();
+            }
+            for (int i = 0; i < consumers.Length; i++) {
+                consumers[i].Consume();
+            }
+
+            for (int i = 0; i < consumers.Length; i++) {
+                consumers[i].Join();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+    }
+}
